98396aa1017aff57b8b19ee373477749117bfff9
Dhruv Gamdha <dgamdha@iastate.edu>
2021-06-11 21:43:14 -0500

    Updated parameters

--- main.py
+++ main.py
@@ -31,16 +31,16 @@ print("\n d space:", dspace_nm)
 dspace_pix = int(dspace_nm*pix2nm)
 # print("dspace_pix:", dspace_pix)
 
-blur_iteration          = 200                   # Best = 4 prev Best = 200
-dspace_frac_Blur_kernel = int(0.09*dspace_pix)                  # Best = 0.5, prev Best 0.09, Fraction of dspace for the blur kernel size 
+blur_iteration          = 15                    # Best = 4 prev Best = 200
+dspace_frac_Blur_kernel = int(0.15*dspace_pix)  # Best = 0.5, prev Best 0.09, Fraction of dspace for the blur kernel size 
 closing_k_size          = 15                    # Kernel Size
 opening_k_size          = 17                    # Kernel Size
-pixThresh               = int(0.625*dspace_pix)  # 1.25*dspace_pix,Threshold number of pixels consituting polymers
+pixThresh               = int(0.625*dspace_pix) # 1.25*dspace_pix,Threshold number of pixels consituting polymers
 ellipse_len             = int(1.5*dspace_pix)   # Old = 160,Breaking polymer into this size before constructing ellipse 
 ellipseAspectRatio      = 5                     # Threshold aspect Ratio of the ellipse
-thresh_dist             = int(1.35*dspace_pix)  # Old = 250, Distance threshold for adjacency matrix 
+thresh_dist             = int(2*dspace_pix)     # Old = int(1.35*dspace_pix), Distance threshold for adjacency matrix 
 thresh_theta            = 10                    # delta Theta threshold for adjacency matrix 
-clusterSize             = 10                    # old = 10, Threshold Crystal cluster size  
+clusterSize             = 7                    # old = 10, Threshold Crystal cluster size  
 
 
 debug               = 0     # To show images: 1, Not to:0
