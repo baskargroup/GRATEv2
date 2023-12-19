b36e12c545a97b47a426a73f0976767556b18630
bglab-dgamdha <dgamdha@iastate.edu>
2023-12-18 20:17:38 -0600

    Refactor GRATE function and extract sub-functions for better code organization and readability

--- grate.py
+++ grate.py
@@ -64,11 +64,59 @@ def process_ellipse_construction(Broken_backbone_img, parameters):
         print_time("Ellipse Construction", start_time)
     return bb_ellipse1, bb_ellipse_props
 
+def process_adjacency_mat(Broken_backbone_img, bb_ellipse_props, parameters):
+    timeCode = parameters.get('timeCode', 0)
+    start_time = time.time()
+    adjacencyMat = AdjacencyMat(Broken_backbone_img, bb_ellipse_props, parameters)
+    if timeCode:
+        print_time("Adjacency Matrix", start_time)
+    return adjacencyMat
+
+def process_connec_comp(Broken_backbone_img, adjacencyMat, bb_ellipse_props, parameters, img_path):
+    timeCode = parameters.get('timeCode', 0)
+    start_time = time.time()
+    ellipseCluster, AllClusterPointCloud, crystalAngles = ConnecComp(Broken_backbone_img, adjacencyMat, bb_ellipse_props, parameters, img_path)
+    if timeCode:
+        print_time("Connected Component", start_time)
+    return ellipseCluster, AllClusterPointCloud, crystalAngles
+
+def process_plotting_and_saving(img, AllClusterPointCloud, img_path, crystalAngles, parameters):
+    timeCode = parameters.get('timeCode', 0)
+    start_time = time.time()
+    crystalArea, centroid, crystalAngles_final, dspaces, df_boundBox, crystalMajorAxis_length, crystalMinorAxis_length, crystalMajorAxisAngle, angleDifference = PlottingAndSaving(img, AllClusterPointCloud, img_path, crystalAngles, parameters)
+    if timeCode:
+        print_time("Eval D-Spacing, Plotting and Saving", start_time)
+    return crystalArea, centroid, crystalAngles_final, dspaces, df_boundBox, crystalMajorAxis_length, crystalMinorAxis_length, crystalMajorAxisAngle, angleDifference
+
+def process_and_save_dataframe(img_path, parameters, centroid, crystalArea, crystalAngles_final, dspaces, crystalMajorAxis_length, crystalMinorAxis_length, crystalMajorAxisAngle, angleDifference):
+    
+    if len(centroid) == 0:
+        imgNamelist = [img_path.name]
+    else:
+        imgNamelist = [None] * len(centroid)
+        imgNamelist[0] = img_path.name
+
+    df = pd.DataFrame(list(zip(imgNamelist, centroid, crystalArea, crystalAngles_final, dspaces, crystalMajorAxis_length, crystalMinorAxis_length, crystalMajorAxisAngle, angleDifference)), 
+                      columns=['Image Name', 'Centroid', 'Crystal Area (nm^2)', 'Crystal Angle (zero at X-axis and clockwise positive)', 'D-Spacing(FFT, nm)', 'crystalMajorAxis_length (nm)', 'crystalMinorAxis_length (nm)', 'MajorAxisAngle', 'angleDifference'])
+    df.round(2)
+    
+    csv_file_path = Path(parameters['result CSV directory']) / f"{img_path.stem}.csv"
+    df.to_csv(csv_file_path)
+    
+    return df
+
+def process_save_backbone_coords(img_path, parameters, df_boundBox):
+    
+    if parameters['save bounding box'] == 1:
+        df_BB = pd.DataFrame( list( zip( df_boundBox ) ) , columns = [ "Top Left(x_y) Bottom Right(x_y)" ] )
+        df_BB.to_csv(join(parameters['result annotation directory'], img_path.stem+'.csv'))
+    
+
+
 def GRATE(img_path, parameters):
     # img = cv2.imread(join(imgName),0)
     img = io.imread(img_path)
     img = img.astype('float64')
-    timeCode = 0
     
     thresh = process_image(img, parameters)
     
@@ -82,100 +130,14 @@ def GRATE(img_path, parameters):
     
     bb_ellipse1, bb_ellipse_props = process_ellipse_construction(Broken_backbone_img, parameters)
     
-    # t0 = time.time()
-    # thresh = BlurThresh(img, parameters)
-    # if timeCode == 1:
-    #     t1 = time.time()
-    #     total = t1-t0
-    #     print("Blurring and Thresholding Time           :", round(total,2))
-
-    # # t0 = time.time()
-    # if parameters['d space pix'] < 78:
-    #     skeleton = Skeletonize(thresh, parameters)
-    # else:
-    #     closing = Closing(thresh, parameters)
-    #     opening = Opening(closing, parameters)
-    #     skeleton = Skeletonize(opening, parameters)
+    adjacencyMat = process_adjacency_mat(Broken_backbone_img, bb_ellipse_props, parameters)
     
-    # if timeCode == 1:
-    #     t0 = time.time()
-    #     total = t0-t1
-    #     print("Closing Opening and Skeletonization Time :", round(total,2))
-
-    # t0 = time.time()
-    # skeleton = BreakBranches(skeleton, parameters)
-    # if timeCode == 1:
-    #     t1 = time.time()
-    #     total = t1-t0
-    #     print("Breaking Banches Time                    :", round(total,2))
+    ellipseCluster, AllClusterPointCloud, crystalAngles = process_connec_comp(Broken_backbone_img, adjacencyMat, bb_ellipse_props, parameters, img_path)
     
-    # t0 = time.time()
-    # # _, temp = SkeletonSegmentation(skeleton)
-    # temp = SkeletonSegmentation(skeleton)
-    # if timeCode == 1:
-    #     t1 = time.time()
-    #     total = t1-t0
-    #     print("Segmentation Time                        :", round(total,2))
-
-    # Broken_backbone_img = Filtered_Uniform_BB(img, temp, parameters)
-    # if timeCode == 1:
-    #     t0 = time.time()
-    #     total = t0-t1
-    #     print("Uniform BB Time                          :", round(total,2))
-
-    ######################### MODIFY Filtered_Uniform_BB TO REMOVE RESIDUAL ALSO ################################
-#     _, filteredPolys = SkeletonSegmentation(Broken_backbone_img_temp)
-#     Broken_backbone_img = Remove_small_BB(Broken_backbone_img_temp, filteredPolys, int(0.5*ellipse_len))
+    crystalArea, centroid, crystalAngles_final, dspaces, df_boundBox, crystalMajorAxis_length, crystalMinorAxis_length, crystalMajorAxisAngle, angleDifference = process_plotting_and_saving(img, AllClusterPointCloud, img_path, crystalAngles, parameters)
     
-#     _, filteredPolys1 = SkeletonSegmentation(Broken_backbone_img)
+    df = process_and_save_dataframe(img_path, parameters, centroid, crystalArea, crystalAngles_final, dspaces, crystalMajorAxis_length, crystalMinorAxis_length, crystalMajorAxisAngle, angleDifference)
     
-#     restructuredFP = RemovingDim(filteredPolys1)
+    process_save_backbone_coords(img_path, parameters, df_boundBox)
     
-    # t0 = time.time()
-    # bb_ellipse1, bb_ellipse_props = EllipseConstruction(Broken_backbone_img, parameters)
-    # if timeCode == 1:
-    #     t1 = time.time()
-    #     total = t1-t0
-    #     print("Ellipse Construction Time                :", round(total,2))
-
-
-    # t0 = time.time()
-    adjacencyMat = AdjacencyMat(Broken_backbone_img, bb_ellipse_props, parameters)
-    if timeCode == 1:
-        t0 = time.time()
-        total = t0-t1
-        print("Adjacency Matrix Time                    :", round(total,2))
-
-    # t0 = time.time()
-    ellipseCluster, AllClusterPointCloud, crystalAngles = ConnecComp(Broken_backbone_img, adjacencyMat, bb_ellipse_props, parameters, img_path)
-    if timeCode == 1:
-        t1 = time.time()
-        total = t1-t0
-        print("Connected Component Time                 :", round(total,2))
-
-    # t0 = time.time()
-    crystalArea, centroid, crystalAngles_final, dspaces, df_boundBox, crystalMajorAxis_length, crystalMinorAxis_length, crystalMajorAxisAngle, angleDifference = PlottingAndSaving(img, AllClusterPointCloud, img_path, crystalAngles, parameters)
-    if timeCode == 1:
-        t0 = time.time()
-        total = t0-t1
-        print("Eval D-Spacing, Plotting and Saving Time :", round(total,2))
-
-
-    if len(centroid) == 0:
-        imgNamelist = [img_path.name]
-    else:
-        imgNamelist = [None]*len(centroid)
-        imgNamelist[0] = img_path.name
-    
-    df = pd.DataFrame(list(zip(imgNamelist, centroid, crystalArea, crystalAngles_final, dspaces, crystalMajorAxis_length, crystalMinorAxis_length, crystalMajorAxisAngle, angleDifference)), columns=['Image Name','Centroid', 'Crystal Area (nm^2)', 'Crystal Angle (zero at X-axis and clockwise positive)', 'D-Spacing(FFT, nm)', 'crystalMajorAxis_length (nm)', 'crystalMinorAxis_length (nm)', 'MajorAxisAngle', 'angleDifference'])
-    df = df.round(2)
-
-#     print("Saving results to: ",join(projectPath, resultDir,imgName[:-4]+'.csv'))
-    df.to_csv(join(parameters['result CSV directory'], img_path.stem+'.csv'))
-    
-    if parameters['save bounding box'] == 1: 
-#         print("Saving BB Annotations to: ",join(projectPath, annotationDir, imgName[:-4]+'.csv'))
-        df_BB = pd.DataFrame( list( zip( df_boundBox ) ) , columns = [ "Top Left(x_y) Bottom Right(x_y)" ] )
-        df_BB.to_csv(join(parameters['result annotation directory'], img_path.stem+'.csv'))
-    
-    return df 
+    return df
